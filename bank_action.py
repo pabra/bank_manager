a57871680b498edf3b921b715866aa1048a5f15e
pabra <pabra@netzfruech.de>
2014-12-21 10:22:33 +0100

    test

--- bank_action.py
+++ bank_action.py
@@ -260,8 +260,8 @@ def send_message(msg=[], subject='Debit Warning', mail_from='patrick.braune@gmai
     text_to_send = text_to_send.decode('utf-8')
     try:
         smtp_msg = MIMEText(text_to_send)
-        smtp_msg.set_charset('utf-8')
-        smtp_msg['Subject'] = subject
+        #smtp_msg.set_charset('utf-8')
+        smtp_msg['Subject'] = 'üöäß'+subject
         smtp_msg['From'] = mail_from
         smtp_msg['To'] = rcpt_to
         s = smtplib.SMTP(host)
