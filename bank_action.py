cd74816d1f6f8b65bb05c1901f8ba5ba248aaf5a
pabra <pabra@netzfruech.de>
2015-03-23 22:21:55 +0100

    use mechanize instead of requests

--- bank_action.py
+++ bank_action.py
@@ -4,29 +4,24 @@
 import os
 import sys
 import re
-import time
 import csv
 import sqlite3
 import locale
 import datetime
 import pprint
-import requests
 import ConfigParser
+import mechanize
 import smtplib
 from email.mime.text import MIMEText
-from urlparse import urljoin
-from bs4 import BeautifulSoup
 
 DB_CONNECTION = False
 
 locale.setlocale(locale.LC_MONETARY, locale.getdefaultlocale())
 
-class Session(object):
+class Config(object):
     def __init__(self, account):
         self.account = account
         self._load_config()
-        self.last_url = None
-        self.cookies = {}
 
     def _load_config(self):
         self.Config = ConfigParser.ConfigParser()
@@ -45,50 +40,6 @@ class Session(object):
 
     def conf(self, param):
         return self.Config.get(self.account, param)
-
-    def get(self, location, post=False, post_data=None, track_last_url=True, binary=False):
-        url = urljoin(self.last_url, location)
-        kwargs = {}
-        #kwargs['headers'] = {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'}
-
-        if self.cookies:
-            kwargs['cookies'] = self.cookies
-
-        # always sleep 5 sec
-        time.sleep(5)
-
-        if post:
-            r = requests.post(url, data=post_data, **kwargs)
-        else:
-            r = requests.get(url, **kwargs)
-
-        assert r.ok
-
-        for h in r.history or () + (r,):
-            if h.cookies:
-                self.cookies.update(h.cookies.get_dict())
-
-        if track_last_url:
-            self.last_url = r.request.url
-
-        if not binary:
-            cnt = r.text.encode('utf8')
-            if not cnt:
-                pprint.pprint(r.headers)
-                pprint.pprint(r.history)
-            return cnt
-
-        return r.content
-
-    def logout(self, content):
-        try:
-            soup = BeautifulSoup(content)
-            logout = soup.find('li', class_='logout').find('a').get('href')
-        except AttributeError:
-            assert False, 'Could not find logout link. Maybe we are not logged in.'
-
-        content = self.get(logout)
-        save('log/logged_out.html', content)
 
 def get_cwd():
     return os.path.dirname(os.path.realpath(__file__))
@@ -446,14 +397,14 @@ def drop_entries_for_date(account, last_date):
         cur.execute(q, (account, last_date))
 
 def update_db(account):
-    sess = Session(account)
-    acc_no = sess.conf('user')
+    config = Config(account)
+    acc_no = config.conf('user')
     check_account_existence(account, acc_no)
     last_date = get_last_entry_date(acc_no)
     files = get_files(account=account, min_date=last_date)
     files.sort()
     for f in files:
-        db_import_file(f, acc_no, sess)
+        db_import_file(f, acc_no, config)
 
     db_close()
 
@@ -461,82 +412,73 @@ def fetch(account):
     date_format = '%d.%m.%Y'
     date_save_format = '%Y_%m_%d'
 
-    sess = Session(account)
-    content = sess.get('https://banking.postbank.de')
-    save('log/login.html', content)
+    config = Config(account)
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.set_handle_refresh(False)
+    br.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0')]
+    br.open('https://banking.postbank.de')
+
+    save('log/login.html', br.response().read())
     #content = read('log/login.html')
-    soup = BeautifulSoup(content)
-    login_form = soup.find('form', class_='form-cn')
-    login_data = {}
-    for inp in login_form.find_all(['input', 'button']):
-        login_data[inp.get('name')] = inp.get('value')
+    br.form = filter(lambda f: f.attrs.get('class') == 'form-cn', br.forms())[0]
+    br['nutzername'] = config.conf('user')
+    br['kennwort'] = config.conf('pass')
+    br.submit()
 
-    login_data['nutzername'] = sess.conf('user')
-    login_data['kennwort'] = sess.conf('pass')
-
-    # login
-    content = sess.get(login_form.get('action'), post=True, post_data=login_data)
-    save('log/logged_in.html', content)
+    save('log/logged_in.html', br.response().read())
     #content = read('log/logged_in.html')
-    soup = BeautifulSoup(content)
-    transactions = soup.find('li', class_='ng-transactions').find('a').get('href')
+    link = filter(lambda l: l.text == 'Umsätze', br.links())[0]
+    br.follow_link(link)
 
-    # get transactions
-    content = sess.get(transactions)
-    save('log/transcations.html', content)
+    save('log/transcations.html', br.response().read())
     #content = read('log/transcations.html')
-    soup = BeautifulSoup(content)
-    trans_form = soup.find('form')
-    trans_data = {}
-    for sel in trans_form.find_all('select'):
-        for opt in sel.find_all('option'):
-            if not sel.get('value', 1) or opt.get('selected'):
-                sel['value'] = opt.get('value', '')
-
     today = datetime.date.today()
-    trans_form.find('div', class_='fld-date-bis').find('input')['value'] = today.strftime(date_format)
-    trans_form.find('div', class_='fld-date-von').find('input')['value'] = (today - datetime.timedelta(95)).strftime(date_format)
+    br.form = list(br.forms())[0]
+    ctrl = filter(lambda c: c.type == 'text' and re.match(r'^.*?bisDatum$', c.name),  br.form.controls)[0]
+    ctrl.value = today.strftime(date_format)
+    ctrl = filter(lambda c: c.type == 'text' and re.match(r'^.*?vonDatum$', c.name),  br.form.controls)[0]
+    ctrl.value = (today - datetime.timedelta(95)).strftime(date_format)
+    br.submit()
 
-    for el in trans_form.find_all(['input', 'select', 'button']):
-        trans_data[el.get('name')] = el.get('value')
-
-    content = sess.get(trans_form.get('action'), post=True, post_data=trans_data)
-    save('log/transcations-95.html', content)
+    save('log/transcations-95.html', br.response().read())
     #content = read('log/transcations-95.html')
-    if not content:
-        print 'post transaction data again with 3 sec timeout'
-        time.sleep(3)
-        content = sess.get(trans_form.get('action'), post=True, post_data=trans_data)
-        save('log/transcations-95_2.html', content)
-    soup = BeautifulSoup(content)
-    csv_link = soup.find('a', class_='action-pdf').get('href')
-    csv_content = sess.get(csv_link, track_last_url=False)
+    link = filter(lambda l: l.text == 'CSV herunterladen', br.links())[0]
+    link2 = filter(lambda l: l.text == 'Zu den Kontoauszügen', br.links())[0]
+    br.follow_link(link)
+
     save('data/%s/trans/%s_%s.csv' % (today.year,
                                       account,
-                                      today.strftime(date_save_format)), csv_content)
+                                      today.strftime(date_save_format)),
+         br.response().read())
 
-    statements_overview = soup.find('a', class_='action-more').get('href')
+    br.open(link2.absolute_url)
 
-    content = sess.get(statements_overview)
-    save('log/statements_overview.html', content)
+    save('log/statements_overview.html', br.response().read())
     #content = read('log/statements_overview.html')
-    soup = BeautifulSoup(content)
-    pdf_links = []
-    for row in soup.find_all('tr', class_='state-unmarked'):
-        pdf_date = datetime.datetime.strptime(row.find('td', class_='headers-date').get_text(strip=True), date_format).date()
-        pdf_link = row.find('a', 'action-icon-download').get('href')
-        pdf_links.append((pdf_date, pdf_link))
 
-    if pdf_links:
-        for x in pdf_links:
-            pdf_content = sess.get(x[1], track_last_url=False, binary=True)
-            save('data/%s/pdf/%s_%s.pdf' % (x[0].year,
-                                            account,
-                                            x[0].strftime(date_save_format)),
-                 pdf_content,
-                 binary=True)
+    logout_link = filter(lambda l: l.text == 'Banking beenden', br.links())[0]
 
-    sess.logout(content)
+    # ToDo: download PDF files
+    #pdf_links = []
+    #for row in soup.find_all('tr', class_='state-unmarked'):
+    #    pdf_date = datetime.datetime.strptime(row.find('td', class_='headers-date').get_text(strip=True), date_format).date()
+    #    pdf_link = row.find('a', 'action-icon-download').get('href')
+    #    pdf_links.append((pdf_date, pdf_link))
+
+    #if pdf_links:
+    #    for x in pdf_links:
+    #        pdf_content = config.get(x[1], track_last_url=False, binary=True)
+    #        save('data/%s/pdf/%s_%s.pdf' % (x[0].year,
+    #                                        account,
+    #                                        x[0].strftime(date_save_format)),
+    #             pdf_content,
+    #             binary=True)
+
+    br.open(logout_link.absolute_url)
+    save('log/logged_out.html', br.response().read())
+
+    br.close()
 
     cleanup_csv(date_save_format)
 
